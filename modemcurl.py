08a16c7342c32d73d5d7291b0bad7111271371a1
bryllus <brylletc@yahoo.com>
2020-03-25 19:47:24 +0800

    Complete PLDT iGateway. Proper formatting.

--- modemcurl.py
+++ modemcurl.py
@@ -1,3 +1,4 @@
+import ast
 import configparser
 import json
 import pycurl
@@ -26,9 +27,13 @@ class GlobeAztech(ModemCurl):
 		curl.perform() 
 		curl.close()
 
+		# Wrap values in double quotes in order for json.loads to succeed
 		body = bytes.getvalue().decode('utf8', 'ignore').replace("'", '"')
+		# Wrap names in double quotes for json.loads
 		replaced = re.sub("(\s*?{\s*?|\s*?,\s*?)(['\"])?([a-zA-Z0-9_]+)(['\"])?:", '\g<1>"\g<3>":', body)
+
 		self.STATUS = json.loads(replaced)
+		self.print_status()
 	def print_status(self):
 		print("Globe Aztech")
 		print("Status: " + self.STATUS['HOME_Adsl_Status'])
@@ -43,6 +48,10 @@ class TPLinkR470(ModemCurl):
 		configParser.read(configFilePath)
 
 class PLDTiGateway(ModemCurl):
+	def get_data(self, content):
+		result = PLDTiGatewayResult(content)
+		self.STATUS = result.parse()
+
 	def get_status(self):
 		URL = self.URL + 'cgi-bin/webproc'
 		bytes = BytesIO()
@@ -54,29 +63,106 @@ class PLDTiGateway(ModemCurl):
 		username = configParser.get(self.__class__.__name__, 'username')
 		password = configParser.get(self.__class__.__name__, 'password')
 
+		f = open('/dev/null', 'wb')
+
 		post = "getpage=html%2Findex.html&errorpage=html%2Fmain.html&" \
 			"var%3Amenu=setup&var%3Apage=wireless&obj-action=auth&" \
 			"%3Ausername=" + username + "&%3Apassword=" + password + \
-			"&%3Aaction=login&"
+			"&%3Aaction=login"
 
+		# First step is to retrieve the sessionid
+		# The sessionid is required in order for login to go through
 		curl.setopt(curl.URL, URL)
 		curl.setopt(curl.REFERER, self.URL + 'cgi-bin/webproc')
 		curl.setopt(curl.COOKIEFILE, 'cookie')
-		curl.setopt(curl.WRITEDATA, bytes)
+		curl.setopt(curl.WRITEDATA, f)
 		curl.perform()
 
+		cookiejar = MozillaCookieJar()
+		cookiejar.load('cookie')
+
+		sessionid = ''
+		for cookie in cookiejar:
+			if (cookie.name == 'sessionid'):
+				sessionid = cookie.value
+
+		if (sessionid == ''):
+			return
+
+		post += "&%3Asessionid=" + sessionid
+
+		# Try to login with the sessionid
 		curl.setopt(curl.URL, URL)
 		curl.setopt(curl.REFERER, self.URL + 'cgi-bin/webproc')
 		curl.setopt(curl.POSTFIELDS, post)
 		curl.setopt(curl.COOKIEJAR, 'cookie')
 		curl.setopt(curl.FOLLOWLOCATION, 1)
-		curl.setopt(curl.WRITEDATA, bytes)
+		curl.setopt(curl.WRITEDATA, f)
 		curl.perform()
 
-		bytes.seek(0)
+		f.close()
+
+		# Retrieve the status page
 		curl.setopt(curl.URL, URL + "?getpage=html/index.html&var:menu=status&var:page=deviceinfo")
 		curl.setopt(curl.REFERER, self.URL + 'cgi-bin/webproc')
 		curl.setopt(curl.WRITEDATA, bytes)
 		curl.perform()
 
-		print(bytes.getvalue().decode('utf8', 'ignore'))
+		content = bytes.getvalue().decode('utf8', 'ignore')
+
+		if (content):
+			self.get_data(content)
+			self.print_status()
+
+	def print_status(self):
+		print("PLDT iGateway")
+		print("Status: " + self.STATUS['status'])
+		print("Uptime: " + self.STATUS['uptime'])
+		print("Download: " + self.STATUS['download'])
+		print("Upload: " + self.STATUS['upload'])
+		return
+
+class PLDTiGatewayResult:
+	def __init__(self, content):
+		self.content = content
+	def parse(self):
+		result = {
+			'name': '',
+			'status': '',
+			'ip' : '',
+			'uptime' : '',
+			'download' : '',
+			'upload' : ''
+		}
+
+		# Retrieve block of javascript object that contains the information
+		match = re.search("G_wanConnction\[m\] = (\[.+?\])\;", self.content, re.MULTILINE | re.DOTALL)
+		matches = match.groups()
+
+		if(len(matches) == 1):
+			isolated = matches[0]
+			# Remove comments from the block
+			cleansed = re.sub(" ?//[a-zA-Z ]*\r", "", isolated)
+			# Remove the javascript code
+			cleansed = re.sub("==\"[a-zA-Z]*\" \? [a-zA-Z_ \.\:]* ,", ",", cleansed)
+			
+			# Try to eval the cleaned value
+			evaled = ast.literal_eval(cleansed)
+			result['name'] = evaled[0]
+			result['status'] = evaled[1]
+			result['ip'] = evaled[8]
+			result['uptime'] = evaled[14]
+
+		# Retrieve download rate
+		match = re.search("G_dsl_downrate = \"(\d+)\"", self.content)
+		matches = match.groups()
+		if(len(matches) == 1):
+			result['download'] = matches[0]
+
+		# Retrieve upload rate
+		match = re.search("G_dsl_uprate = \"(\d+)\"", self.content)
+		matches = match.groups()
+		if(len(matches) == 1):
+			result['upload'] = matches[0]		
+
+		return result
--- modemstatus.py
+++ modemstatus.py
@@ -3,8 +3,6 @@ from  modemcurl import *
 
 pldt = PLDTiGateway('http://192.168.1.1/')
 pldt.get_status()
-#tplink.print_status()
-
-#globe = GlobeAztech('http://192.168.254.254/')
-#globe.get_status()
-#globe.print_status()
+print("")
+globe = GlobeAztech('http://192.168.254.254/')
+globe.get_status()
