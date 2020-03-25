d309d0b22ed56afd6f9e794ce8e3eac1b4c34743
bryllus <brylletc@yahoo.com>
2020-03-25 20:53:56 +0800

    Add threading because we're fancy.

--- modemstatus.py
+++ modemstatus.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
+import threading
 from  modemcurl import *
 
-pldt = PLDTiGateway('http://192.168.1.1/')
-pldt.get_status()
-print("")
-globe = GlobeAztech('http://192.168.254.254/')
-globe.get_status()
+def startPLDT():
+	pldt = PLDTiGateway('http://192.168.1.1/')
+	pldt.get_status()
+
+def startGlobe():
+	globe = GlobeAztech('http://192.168.254.254/')
+	globe.get_status()
+
+pldt = threading.Thread(target=startPLDT)
+globe = threading.Thread(target=startGlobe)
+
+pldt.start()
+globe.start()
